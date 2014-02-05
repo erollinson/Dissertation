6b378b7b74cbc6e78092f9df9d8d0fe4d712ce1a
erollinson <ejrollinson@gmail.com>
2014-02-05 18:15:54 -0500

    working on adding stress test

--- ordination script.R
+++ ordination script.R
@@ -2,6 +2,8 @@
 #columns should all be numeric
 #define row names
 
+comm2<-read.csv("2012_abundancematrix.csv")
+
 #use this one to give the site names
 rownames(comm2) <- c("B1GA", "B1GB", "BIUA", "B1UB", "B2GA", "B2GB", "B2UA", "B2UB", "I1GA", "I1GB", "I1UA", "I1UB", "I2GA", "I2GB", "I2UA", "I2UB", "K1GA", "K1GB", "K1UA", "K1UB", "K2GA", "K2GB", "K2UA", "K2UB")
 
@@ -10,7 +12,7 @@ rownames(comm2) <- c("77", "17", "177", "117", "82", "19", "182", "119", "65", "
 
 
 #run the NMDS
-
+library(vegan)
 ord <- metaMDS(comm2)
 
 #plot the ordination without species showing, and with sites shown in text
