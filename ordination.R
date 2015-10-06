bfbdedd9bd855091c7ef2d0bfe6297868cc3044e
erollinson <ejrollinson@gmail.com>
2015-10-05 20:54:06 -0400

    substantial additions to introduced species traits chapter. posthoc analysis of traits differences, figures for every trait. addition of anosim within annual and within perennial, and figures for same.

--- ordination.R
+++
@@ -1,6 +0,0 @@
-#transform data frame to site x species matrix
-
-library(reshape)
-cquada <- cast(cquad, Name + Quad ~ Species, value='Count', FUN=mean)
-cquada <-as.data.frame(cquada)
-cquada[is.na(cquada)] = 0
